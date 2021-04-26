cb5e6676339fa6b48b6b821360018d74a6c1169a
mamota919 <61941836+mamota919@users.noreply.github.com>
2021-04-26 18:27:34 -0400

    AirPython

--- New folder/airpython.py
+++ New folder/airpython.py
@@ -165,18 +165,18 @@ def open_scope():
 
 
     def open_ScopeSettings():
-    fenster = Tk()
-    fenster.title("ScopeSettings")
-    def switch():
-        if b0["state"] == "normal":
-            b0["state"] = "disabled"
-            b2["text"] = "enable"
-        elif b3["state"] == "normal":
-            b0["state"] = "disabled"
-            b2["text"] = "ClickHere"
-        else:
-            b2["state"] = "normal"
-            b2["text"] = "disable"
+        fenster = Tk()
+        fenster.title("ScopeSettings")
+        def switch():
+            if b0["state"] == "normal":
+                b0["state"] = "disabled"
+                b2["text"] = "enable"
+            elif b3["state"] == "normal":
+                b0["state"] = "disabled"
+                b2["text"] = "ClickHere"
+            else:
+                b2["state"] = "normal"
+                b2["text"] = "disable"
 
     #--Buttons
     b0 = Button(fenster, text="Settings", height=8, width=8)
