cea6dd80084a1a359215b29c63d5650c6bcf075d
mamota919 <61941836+mamota919@users.noreply.github.com>
2021-04-30 16:18:09 -0400

    timer

--- timer.py
+++ timer.py
@@ -1,20 +1,66 @@
-from tkinter import*
-from tkinter.ttk import*
+import tkinter as Tkinter 
+from datetime import datetime
+counter = 0
+running = False
+def counter_label(label): 
+    def count(): 
+        if running: 
+            global counter 
 
-from time import strftime
+            if counter==0:             
+                display=" "
+            else:
+                tt = datetime.fromtimestamp(counter)
+                string = tt.strftime("%M:%S")
+                display=string 
+    
+            label['text']=display
+    
+            
+            label.after(900, count)  
+            counter += 1
+    
+    count()      
+    
+def Start(label): 
+    global running 
+    running=True
+    counter_label(label) 
+    start['state']='disabled'
+    stop['state']='normal'
+    reset['state']='normal'
 
-root = Tk()
-root.title("Time")
+def Stop(): 
+    global running 
+    start['state']='normal'
+    stop['state']='disabled'
+    reset['state']='normal'
+    running = False
 
-def time ():
-    string = strftime("%H:%M:%S %p")
-    label.config(text=string)
-    label.after(1000,time)
+def Reset(label): 
+    global counter 
+    counter=0
 
-label = Label(root, font=("ds-digital", 80))
-label.pack(anchor = "center")
-time()
+    if running==False:       
+        reset['state']='disabled'
+        label['text']=' '
 
-print("Arrival Time: ", )
-print("Depature Time: ", )
-mainloop()
+    else:                
+        label['text']=' '
+    
+root = Tkinter.Tk() 
+root.title("Stopwatch") 
+    
+
+root.minsize(width=250, height=70) 
+label = Tkinter.Label(root, text=" ", fg="black", font="Verdana 30 bold") 
+label.pack() 
+f = Tkinter.Frame(root)
+start = Tkinter.Button(f, text='Start', width=6, command=lambda:Start(label)) 
+stop = Tkinter.Button(f, text='Stop',width=6,state='disabled', command=Stop) 
+reset = Tkinter.Button(f, text='Reset',width=6, state='disabled', command=lambda:Reset(label)) 
+f.pack(anchor = 'center',pady=5)
+start.pack(side="left") 
+stop.pack(side ="left") 
+reset.pack(side="left") 
+root.mainloop()
