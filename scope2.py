1ea17fe2b02ac9970c3504df17e3eb52f00f330c
Jose Casiano <josecasiano@joses-mbp.myfiosgateway.com>
2021-05-02 20:01:36 -0400

    Flights

--- scope2.py
+++ scope2.py
@@ -5,7 +5,7 @@ API_KEY = 'AIzaSyC91eORDd5nWBrVCsmUCK3e4cnpYn5d9DQ'
 
 map_client = googlemaps.Client(API_KEY)
 
-work_place_address = 'INSERT ADDRESS HERE'
+work_place_address = '800 Essington Ave, Philadelphia, PA'
 response = map_client.geocode(work_place_address)
 pprint(response)
 print(response[0]['geometry'])
