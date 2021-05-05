e35cbe034ca5d23cd673c02468959c9ee5b35601
Alex Weber <arw5670@psu.edu>
2021-05-05 18:21:19 -0400

    app

--- database.py
+++ database.py
@@ -2,12 +2,14 @@ import mysql.connector
 db = mysql.connector.connect(user= "root", passwd= "alexweber99", host = "localhost", database = "testdatabase")
 
 mycursor= db.cursor()
-#mycursor.execute("INSERT INTO flights (FlightID, airline, origin, destination) VALUES (%s, %s, %s, %s)", ("DA4564", "Delta", "JFK", "PHL"))
-
-mycursor.execute("DESCRIBE flights")
+#mycursor.execute ("ALTER TABLE flights ADD PrimaryKey int PRIMARY KEY AUTO_INCREMENT")
+#mycursor.execute ("ALTER TABLE flights DROP PrimaryKey")
+#mycursor.execute("INSERT INTO flights (FlightID, airline, origin, destination) VALUES (%s, %s, %s, %s)", ("AA1439", "American Airlines", "ACY", "PHL"))
+#db.commit()
+#mycursor.execute("DESCRIBE flights")
 mycursor.execute("SELECT * FROM flights")
-#for x in mycursor:
-    #print (x)
+for x in mycursor:
+    print (x)
 
 
 
